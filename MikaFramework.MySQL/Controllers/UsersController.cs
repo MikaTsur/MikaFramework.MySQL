ec4627515c999a0912ff3a2d8cd1e4caee4d32c3
morellyo <morellyo@DESKTOP-728KA4M>
2024-02-03 21:48:36 +0200

    Add project files.

---
+++ MikaFramework.MySQL/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using MikaFramework.MySQL.Data;
+using MikaFramework.MySQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace MikaFramework.MySQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+       private readonly AppDbContext _appDbContext;
+       public UsersController(AppDbContext appDbContext) 
+        { 
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user)
+        {
+            _appDbContext.Users.Add(user);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(user);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _appDbContext.Users.ToListAsync();
+            return Ok(users);
+
+        }
+
+    }
+}
---
+++ MikaFramework.MySQL/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace MikaFramework.MySQL.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public int NumOfActions { get; set; }
+
+
+    }
+}
